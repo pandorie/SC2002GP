ce0a1cc47466b7713c0aff7b94c037e6d2a512bf
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-07 00:13:03 +0800

    Start of project

---
+++ src/Combatant.java
@@ -0,0 +1,22 @@
+public class Combatant {
+    private String Name;
+    private int Hp;
+    private int maxHp;
+    private int atk;
+    private int def;
+    private int spd;
+//    private effects<>  implement when effects is added
+
+}
+
+public void takeDamage(int damage){
+
+}
+
+public void Heal(int amount){
+
+}
+
+public void applyEffect(e statusEffect){
+
+}
