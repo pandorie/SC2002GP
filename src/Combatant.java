806fdf7927a6e264e5cd25fca3baf31f74c2506c
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-07 00:32:12 +0800

    added combatant empty methods, getters and setters

--- src/Combatant.java
+++ src/Combatant.java
@@ -1,5 +1,5 @@
 public abstract class Combatant {
-    private String Name;
+    private String name;
     private int Hp;
     private int maxHp;
     private int atk;
@@ -9,7 +9,7 @@ public abstract class Combatant {
 
 
     public Combatant(String name, int hp, int maxhp, int atk, int def, int spd){
-        this.Name = name;
+        this.name = name;
         this.Hp = hp;
         this.maxHp = maxhp;
         this.atk = atk;
@@ -26,8 +26,73 @@ public abstract class Combatant {
 
     }
 
-    public void applyEffect(e statusEffect){
+//    public void applyEffect(e statusEffect){
+//
+//    }
+
+    public void tickEffects(){
 
     }
 
+    public boolean isAlive(){
+        return true;
+    }
+
+    public boolean isStunned(){
+        return true;
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHp(){
+        return this.Hp;
+    }
+
+    public int getMaxHp(){
+        return this.maxHp;
+    }
+
+    public int getAtk(){
+        return this.atk;
+    }
+
+    public int getDef(){
+        return this.def;
+    }
+
+    public int getSpd(){
+        return this.spd;
+    }
+
+
+
+    //setters
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHp(int hp) {
+        Hp = hp;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public void setSpd(int spd) {
+        this.spd = spd;
+    }
 }
