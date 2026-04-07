476d2116a824f4611b29982bbc06fc0ec0cd3bb6
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-07 16:05:55 +0800

    added StatusEffect

---
+++ src/StatusEffect.java
@@ -0,0 +1,21 @@
+public abstract class StatusEffect {
+    private int duration;
+
+    public StatusEffect(){
+        this.duration = 2;
+    }
+
+    public void apply(Combatant c){
+
+    }
+
+    public void tick(Combatant c){
+
+    }
+
+    public void isExpired(){
+
+    }
+
+
+}
