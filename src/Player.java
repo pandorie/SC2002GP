ed3ad15c988d15cec3c638164716e23e5d288172
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-07 13:59:11 +0800

    add the remaining methods but still missing item methods as item is not implemented yet

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,14 @@
 public abstract class Player extends Combatant {
     //private List<Item> inventory add after item is added
     private int skillCooldown;
+    private String skillName;
 
-    public Player(String name, int hp, int maxhp, int atk, int def, int spd) {
+    public Player(String name, int hp, int maxhp, int atk, int def, int spd,String skillname) {
         super(name,hp,maxhp,atk,def,spd);
 //      this.inventory = new ArrayList; uncomment ater adding inventory
         this.skillCooldown = 0;
+        this.skillName = skillname;
+
     }
 
 //    public void useItem(item item){
@@ -16,5 +19,19 @@ public abstract class Player extends Combatant {
 
     }
 
+    public void decrementCoolDown(){
 
+    }
+
+    public void getItem(){
+
+    }
+
+    public int getSkillCooldown() {
+        return skillCooldown;
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
 }
