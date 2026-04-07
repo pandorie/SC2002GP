1450bb0dd69d0d2185b35c55839bae8223aec27d
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-07 09:36:00 +0800

    added player class, uncomment useitem and item<list> attribute after implementing items class

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public abstract class Player {
+    //private item items<> add after item is added
+    private int skillCooldown;
+
+    public Player(int skillCooldown) {
+        this.skillCooldown = skillCooldown;
+    }
+
+//    public void useItem(item item){
+//
+//    } uncomment after item is added
+
+    public void executeSpecialSkill(){
+
+    }
+
+
+}
