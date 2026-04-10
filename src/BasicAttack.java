c2b3f0f1e5965bb9ed20b48f89aab4ec4f5c6cd8
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 14:14:46 +0800

    Added BasicAttack Logic

--- src/BasicAttack.java
+++ src/BasicAttack.java
@@ -4,7 +4,14 @@ public class BasicAttack implements Action{
 
     @Override
     public void execute(Combatant source, List<Combatant> targets) {
-
+        if(targets.isEmpty()){
+            System.out.println("There is no target for basic attack");
+            return;
+        }
+        Combatant target = targets.get(0);
+        int damage = source.getAtk() - target.getDef();
+        target.takeDamage(damage);
+        System.out.println(source.getName() + "attacks" + target.getName() + " for " + damage );
     }
 
     @Override
