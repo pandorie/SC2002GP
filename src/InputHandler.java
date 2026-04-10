c79852b7521250cb6094d72397fa5b59219c70c8
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 20:18:14 +0800

    Added selectionAction

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class InputHandler {
     private Scanner scanner;
 
-    public InputHandler(){
+    public InputHandler() {
         this.scanner = new Scanner(System.in);
     }
 
-    public Level SelectDifficulty(){
+    public Level SelectDifficulty() {
         Level selectedLevel = null;
         boolean isValid = false;
 
-        do{
+        do {
             System.out.println("Select Difficulty");
             System.out.println("1. Easy");
             System.out.println("2. Medium");
             System.out.println("3. Hard");
             System.out.println("Select a difficulty 1-3");
 
-            try{
+            try {
                 int choice = scanner.nextInt();
                 scanner.nextLine();
 
-                switch (choice){
+                switch (choice) {
                     case 1:
                         selectedLevel = new Easy();
                         isValid = true;
@@ -40,8 +40,7 @@ public class InputHandler {
                     default:
                         System.out.println("Invalid Choice. Please enter 1-3");
                 }
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Please enter a number");
                 scanner.nextLine();
             }
@@ -53,38 +52,129 @@ public class InputHandler {
     }
 
 
-    public Action SelectAction(Player player){
+    public Action SelectAction(Player player) {
         Action selectedAction = null;
         boolean isValid = false;
 
         do {
+            System.out.println("What will " + player.getName() + "do?");
+            System.out.println("1. Basic Attack");
+            System.out.println("2. Defend");
+            System.out.println("3. " + player.getSkillName() + " Cooldown: " + player.getSkillCooldown());
+            System.out.println("4. use Item");
+            System.out.println("Select a Action: 1-4");
 
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        selectedAction = new BasicAttack();
+                        isValid = true;
+                        break;
+                    case 2:
+                        selectedAction = new Defend();
+                        isValid = true;
+                        break;
+                    case 3:
+                        if(player.getSkillCooldown() > 0){
+                            System.out.println("Skill is on cooldown");
+                        }
+                        else{
+                            selectedAction = new SpecialSkill();
+                            isValid = true;
+                        }
+                        break;
+                    case 4:
+                        Item selectedItem = selectItem();
+                        if(selectedItem != null){
+                            selectedAction = new useItem(selectedItem);
+                            isValid = true;
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number");
+                scanner.nextLine();
+            }
         }
         while (!isValid);
         return selectedAction;
     }
 
-    public Item selectItem(){
+    public Item selectItem() {
         Item selectedItem = null;
         boolean isValid = false;
 
-        do{
+        do {
+            System.out.println("Select 2 items to use in the level");
+            System.out.println("1. Potion");
+            System.out.println("2. Smoke Bomb");
+            System.out.println("3. Power Stone");
+            System.out.println("Select a Item: 1-3");
 
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number");
+                scanner.nextLine();
+            }
         }
-        while(!isValid);
-
+        while (!isValid);
         return selectedItem;
 
     }
 
-    public Combatant selectTarget(List<Enemy> enemies){
+    public Combatant selectTarget(List<Enemy> enemies) {
         Combatant selectedTarget = null;
-        boolean isvalid = false;
+        boolean isValid = false;
 
-        do{
+        do {
+            System.out.println("Select 2 items to use in the level");
+            System.out.println("1. Potion");
+            System.out.println("2. Smoke Bomb");
+            System.out.println("3. Power Stone");
+            System.out.println("Select a Item: 1-3");
 
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number");
+                scanner.nextLine();
+            }
         }
-        while (!isvalid);
+        while (!isValid);
 
         return selectedTarget;
 
