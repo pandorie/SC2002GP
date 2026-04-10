539bd41f19cb36b6e696fa4117d0ac1ef47de6e3
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 14:20:57 +0800

    Added useItem logic

--- src/useItem.java
+++ src/useItem.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class useItem implements Action{
+public class useItem implements Action {
+    private Item SelectedItem;
+
+    public useItem(Item SelectedItem) {
+        this.SelectedItem = SelectedItem;
+    }
+
     @Override
     public void execute(Combatant source, List<Combatant> targets) {
-
+        if (source instanceof Player) {
+            Player player = (Player) source;
+            List<Enemy> enemy = new ArrayList<>();
+            for (Combatant c : targets) {
+                if (c instanceof Enemy) {
+                    enemy.add((Enemy) c);
+                }
+            }
+            SelectedItem.use(player, enemy);
+        } else {
+            return;
+        }
     }
 
     @Override
