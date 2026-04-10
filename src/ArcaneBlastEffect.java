0797c54df425fa827b426f14a91860aaca433154
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 15:51:21 +0800

    Added specialskill logic for Wizard and ArcaneBlastEffect logic and class

---
+++ src/ArcaneBlastEffect.java
@@ -0,0 +1,51 @@
+public class ArcaneBlastEffect extends StatusEffect{
+    //New attributes
+    private int stacks;
+    private int bonusAtk;
+    public ArcaneBlastEffect(){
+        super();
+        this.stacks = 1;
+        this.bonusAtk = 10;
+    }
+
+    @Override
+    public void apply(Combatant c) {
+        c.addEffects(this);
+        this.addBonusAttack(c);
+    }
+
+    //New Method to add bonus atk by calling this
+    public void addBonusAttack(Combatant c){
+        c.setAtk(c.getAtk() + bonusAtk);
+    }
+
+    //New Methods to add stack to arcane for bonus atk
+    public void addStack(Combatant c){
+        setStacks(getStacks() + 1);
+        addBonusAttack(c);
+
+    }
+
+    @Override
+    public void tick(Combatant c) {
+        //doesnt decrease so this method is not needed
+    }
+
+    @Override
+    public boolean isExpired() {
+        return false;
+    }
+
+    @Override
+    public void removeEffect(Combatant c) {
+        c.setAtk(c.getAtk() - (stacks * bonusAtk));
+    }
+    //Added get and set for stacks
+    public int getStacks() {
+        return stacks;
+    }
+
+    public void setStacks(int stacks) {
+        this.stacks = stacks;
+    }
+}
--- src/Wizard.java
+++ src/Wizard.java
@@ -8,13 +8,42 @@ public class Wizard extends Player{
     private static final int def = 10;
     private static final int spd = 20;
     private static final String skillname = "Arcane Blast";
+    private int bonus_atk = 0;
 
     public Wizard() {
         super(name, hp, maxhp, atk, def, spd, skillname);
     }
 
     public void executeSpecialSkill(List<Enemy> targets){
-        
+        for(Enemy target: targets){
+            int damage = this.getAtk() - target.getDef();
+            target.takeDamage(damage);
+            System.out.println(this.getName() + "use Arcane Blast " + target.getName() + " for " + damage);
+            if(!target.isAlive()){
+                ArcaneBlastEffect existingbuff = null;
+                for(StatusEffect effect: this.getEffects()){
+                    if(effect instanceof ArcaneBlastEffect){
+                        existingbuff = (ArcaneBlastEffect) effect;
+                        break;
+                    }
+                }
+
+                if(existingbuff != null){
+                    existingbuff.addStack(this);
+                }
+                else{
+                    ArcaneBlastEffect firstStack = new ArcaneBlastEffect();
+                    firstStack.apply(this);
+                }
+            }
+        }
     }
 
+    public int getBonus_atk() {
+        return bonus_atk;
+    }
+
+    public void setBonus_atk(int bonus_atk) {
+        this.bonus_atk = bonus_atk;
+    }
 }
