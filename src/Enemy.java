4358d2d68497394d6c66de1aa5114a2a09397cc8
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 19:14:56 +0800

    updated takeTurn in Enemy class

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,11 +1,12 @@
 public abstract class Enemy extends Combatant {
-//    private EnemyAction behaviour
+    private EnemybehaviourStrategy behaviour;
 
     public Enemy(String name, int hp, int maxhp, int atk, int def, int spd){
         super(name,hp,maxhp,atk,def,spd);
     }
 
-    public void takeTurn(){
-
+    public void takeTurn(BattleEngine context){
+        Action chooseAction = behaviour.decideAction(this, context);
+        System.out.println(this.getName() + "uses " + chooseAction.getName());
     }
 }
