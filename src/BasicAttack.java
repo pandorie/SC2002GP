ea91983aab77d650d3cf1eef7f036282ddcbf1a4
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-10 12:19:27 +0800

    Created class for BasicAttack and useItem

---
+++ src/BasicAttack.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class BasicAttack implements Action{
+
+    @Override
+    public void execute(Combatant source, List<Combatant> targets) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "BasicAttack";
+    }
+}
---
+++ src/useItem.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class useItem implements Action{
+    @Override
+    public void execute(Combatant source, List<Combatant> targets) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "useItem";
+    }
+}
