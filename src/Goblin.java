287e1da11dcde0a0dbcc788cfc6ee61d1d052582
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-13 16:26:32 +0800

    edited goblin and wolf to handle dynamic spawning

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,5 +1,4 @@
 public class Goblin extends Enemy {
-    private static final String NAME = "goblin";
     private static final int HP = 55;
     private static final int MAXHP = 55;
     private static final int ATK = 35;
@@ -7,8 +6,8 @@ public class Goblin extends Enemy {
     private static final int SPD = 25;
 
 
-    public Goblin() {
-        super(NAME, HP, MAXHP, ATK, DEF, SPD);
+    public Goblin(String letter) {
+        super("Goblin" + letter, HP, MAXHP, ATK, DEF, SPD);
         this.setBehaviour(new BasicAttackBehaviour());
     }
 
--- src/Wolf.java
+++ src/Wolf.java
@@ -1,13 +1,12 @@
 public class Wolf extends Enemy {
-    private static final String NAME = "Wolf";
     private static final int HP = 40;
     private static final int MAXHP = 40;
     private static final int ATK = 45;
     private static final int DEF = 5;
     private static final int SPD = 35;
 
-    public Wolf() {
-        super(NAME, HP, MAXHP, ATK, DEF, SPD);
+    public Wolf(String letter) {
+        super("Wolf" + letter, HP, MAXHP, ATK, DEF, SPD);
         this.setBehaviour(new BasicAttackBehaviour());
     }
 }
