fa2ae9f82106d3aa974ee076b246f0f7af698929
pandorie <52999040+pandorie@users.noreply.github.com>
2026-04-15 21:13:29 +0800

    fixed wizard buff applied timing

--- src/Wizard.java
+++ src/Wizard.java
@@ -15,25 +15,38 @@ public class Wizard extends Player{
     }
 
     public void executeSpecialSkill(List<Enemy> targets, GameLog log){
+        int enemykilled = 0;
+
         for(Enemy target: targets){
             int damage = this.getAtk() - target.getDef();
             target.takeDamage(damage);
             log.showActionResult(this.getName() + " use Arcane Blast on " + target.getName() + " for " + damage);
             if(!target.isAlive()){
-                ArcaneBlastEffect existingbuff = null;
-                for(StatusEffect effect: this.getEffects()){
-                    if(effect instanceof ArcaneBlastEffect){
-                        existingbuff = (ArcaneBlastEffect) effect;
-                        break;
-                    }
+                log.showActionResult(target.getName() + " has been defeated");
+                enemykilled+=1;
+                }
+
+                if(enemykilled > 0){
+                    log.showActionResult(this.getName() + " gained " + (enemykilled*10) + " Atk");
+                    ArcaneBlastEffect existingbuff = null;
+                    for(StatusEffect effect: this.getEffects()){
+                        if(effect instanceof ArcaneBlastEffect){
+                            existingbuff = (ArcaneBlastEffect) effect;
+                            break;
+                        }
                 }
 
                 if(existingbuff != null){
-                    existingbuff.addStack(this);
+                    for(int i = 0; i <enemykilled; i++) {
+                        existingbuff.addStack(this);
+                    }
                 }
                 else{
                     ArcaneBlastEffect firstStack = new ArcaneBlastEffect();
                     firstStack.apply(this, log);
+                    for (int i =1; i<enemykilled; i++){
+                        firstStack.addStack(this);
+                    }
                 }
             }
         }
